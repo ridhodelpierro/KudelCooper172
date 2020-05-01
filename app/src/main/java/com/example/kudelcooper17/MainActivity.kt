22c8275e7c5ab232b9807526438dde8223046963
ASUS <kudelcooper@gmail.com>
2020-05-01 11:16:43 +0800

    Perbaikan Implementasi Toast OnClick TextView

--- app/src/main/java/com/example/kudelcooper17/MainActivity.kt
+++ app/src/main/java/com/example/kudelcooper17/MainActivity.kt
@@ -15,8 +15,8 @@ class MainActivity : AppCompatActivity() {
 
         val textView1 = findViewById<TextView>(R.id.textView1)
 
-        textView1.setOnClickListener(View.OnClickListener {
+        textView1.setOnClickListener{
             Toast.makeText(this,"hai :)",Toast.LENGTH_SHORT).show()
-        })
+        }
     }
 }
