05e80f030cc97d859b1ccfbc9c71e570af6808dc
ASUS <kudelcooper@gmail.com>
2020-05-01 13:03:30 +0800

    Penambahan HitungActivity,panggil dengan intent

--- app/src/main/java/com/example/kudelcooper17/MainActivity.kt
+++ app/src/main/java/com/example/kudelcooper17/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.kudelcooper17
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
@@ -14,9 +16,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val textView1 = findViewById<TextView>(R.id.textView1)
+        val button1 = findViewById<Button>(R.id.button1)
 
         textView1.setOnClickListener{
             Toast.makeText(this,"hai :)",Toast.LENGTH_SHORT).show()
+        }
+
+        button1.setOnClickListener {
+            val intent = Intent(this@MainActivity,HitungActivity::class.java)
+            startActivity(intent)
         }
     }
 }
