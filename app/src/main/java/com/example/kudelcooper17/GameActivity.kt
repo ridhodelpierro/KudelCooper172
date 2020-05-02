4b82b93f89b58d0b600a0562fbb507fed68809d6
ASUS <kudelcooper@gmail.com>
2020-05-02 15:45:45 +0800

    Tampilan Activity Game

---
+++ app/src/main/java/com/example/kudelcooper17/GameActivity.kt
@@ -0,0 +1,13 @@
+package com.example.kudelcooper17
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GameActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+
+    }
+}
